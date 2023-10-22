fff939f81d3309b87b6c397130ea3186dac200ab
Finn <finnprice03@gmail.com>
2023-10-22 12:07:40 -0700

    navbar

---
+++ src/views/js/navbar.js
@@ -0,0 +1,16 @@
+$('#navbar').html(
+  '<button id="navbarButton"><img id="icon16" src="../style/icons/bars.svg"></button>' + 
+  '<div id="navbarBackground">' +
+    '<div id="schoolButtons">' +
+      '<a href="chs.html"><img class="navbarImg" src="../style/images/logos/clevelandLogo.png"></img></a><br>' +
+      '<a href="fhs.html"><img class="navbarImg" src="../style/images/logos/franklinLogo.png"></img></a><br>' + 
+      '<a href="ihs.html"><img class="navbarImg" src="../style/images/logos/idaLogo.png"></img></a><br>' +
+    '</div>'
+);
+
+
+
+
+$("#navbarButton").click(function(){
+  $("#navbarBackground").animate({width: 'toggle'}, 100);
+});
