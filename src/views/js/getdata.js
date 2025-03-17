2260ab8fda2a89b53f171acfb1f3846604bc632b
finnmprice <finnprice03@gmail.com>
2025-03-17 14:22:45 -0700

    new home page, simplified features, start of full rework

--- src/views/js/getdata.js
+++ src/views/js/getdata.js
@@ -25,7 +25,7 @@ function getLastUpdated() {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = String(date.getFullYear()).slice(-2);
 
-    return `${day}/${month}/${year}`;
+    return `${month}/${day}/${year}`;
   }
 
   Object.entries(rawTimes).forEach(([school, time]) => {
